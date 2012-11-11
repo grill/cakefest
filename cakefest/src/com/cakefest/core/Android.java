c93f00bab4ba4005f876ec71d50e832d43e8fdc6
grill <gabriel.grill@aon.at>
2012-11-12 00:12:01 +0100

    Hackathon Release

--- cakefest/src/com/cakefest/core/Android.java
+++ cakefest/src/com/cakefest/core/Android.java
@@ -1,17 +1,21 @@
 package com.cakefest.core;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.Transform;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class Android {
@@ -20,6 +24,11 @@ public class Android {
 	private TextureRegion android;
 	private Texture texture;
 	private Sprite sp;
+	private Fixture fix;
+	private Body body;
+	private Vector2 ve;
+	private int size = 128;
+	private boolean pressed = false;
 
 	public Android(World world, OrthographicCamera camera) {
 		this.world = world;
@@ -30,30 +39,47 @@ public class Android {
 		android = new TextureRegion(texture, 0, 0, 511, 596);
 		sp = new Sprite(android);
 		sp.setPosition(0, 0);
-		sp.setSize(128, 128);
+		
+		sp.setSize(size, size);
+		
+		initPhysics();
 	}
 
 	public void initPhysics() {
         BodyDef bodyDef = new BodyDef();  
         bodyDef.type = BodyType.DynamicBody;  
-        bodyDef.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2);  
-        Body body = world.createBody(bodyDef);  
+        bodyDef.position.set(0, -140);  
+        bodyDef.gravityScale = 20;
+        body = world.createBody(bodyDef);  
         CircleShape dynamicCircle = new CircleShape();  
-        dynamicCircle.setRadius(5f);  
+        dynamicCircle.setRadius(60f);
+        dynamicCircle.setPosition(new Vector2(64, 70));
         FixtureDef fixtureDef = new FixtureDef();  
-        fixtureDef.shape = dynamicCircle;  
+        fixtureDef.shape = dynamicCircle;
         fixtureDef.density = 1.0f;  
         fixtureDef.friction = 0.0f;  
         fixtureDef.restitution = 1;  
-        body.createFixture(fixtureDef);  
+        fix = body.createFixture(fixtureDef); 
+        fix.setUserData(this);
 	}
 	
 	public void draw(SpriteBatch batch) {
 		//batch.draw(android, 0, 0, 511, 596, 400, 400, 1, 1, 0);
+		ve = body.getPosition();
+		sp.setPosition(ve.x, ve.y);
+		sp.setSize(size, size);
 		sp.draw(batch);
+		if (Gdx.input.isKeyPressed(Keys.DPAD_LEFT) || Gdx.input.isKeyPressed(Keys.A)) body.setLinearVelocity(new Vector2(-100, 0));
+		else if (Gdx.input.isKeyPressed(Keys.DPAD_RIGHT) || Gdx.input.isKeyPressed(Keys.D)) body.setLinearVelocity(new Vector2(100, 0));
+		else body.setLinearVelocity(new Vector2(0,0));
+		
 	}
 
 	public void dispose() {
 		texture.dispose();
+	}
+
+	public void fat() {
+		size += 5;
 	}
 }
