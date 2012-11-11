daab82c839b23b7fdae1cfd3b35cb8b22854b6be
grill <gabriel.grill@aon.at>
2012-11-11 15:05:19 +0100

    Again

---
+++ cakefest-desktop/src/com/cakefest/core/Main.java
@@ -0,0 +1,16 @@
+package com.cakefest.core;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "cakefest";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new CakeFestGame(), cfg);
+	}
+}
